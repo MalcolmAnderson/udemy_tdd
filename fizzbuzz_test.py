5a8c1ce9a8151282311a8125e328183b4c7ca9ae
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2022-12-22 05:08:12 +0000

    fizzbuzz done

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -2,7 +2,14 @@ import pytest
 
 
 def fizzBuzz(value):
-    return value
+    retval = ""
+    if value % 3 == 0:
+        retval = "Fizz"
+    if value % 5 == 0:
+        retval += "Buzz"
+    if retval == "":
+        retval = str(value)
+    return retval
 
 
 def test_canCallFizzBuzz():
@@ -10,8 +17,36 @@ def test_canCallFizzBuzz():
 
 
 def test_1_should_return_1():
-    assert fizzBuzz(1) == 1
+    assert fizzBuzz(1) == "1"
 
 
 def test_2_should_return_2():
-    assert fizzBuzz(2) == 2
+    assert fizzBuzz(2) == "2"
+
+
+def test_3_should_return_Fizz():
+    assert fizzBuzz(3) == "Fizz"
+
+
+def test_5_should_return_Buzz():
+    assert fizzBuzz(5) == "Buzz"
+
+
+def test_6_should_return_Fizz():
+    assert fizzBuzz(6) == "Fizz"
+
+
+def test_10_should_return_Buzz():
+    assert fizzBuzz(10) == "Buzz"
+
+
+def test_15_should_return_FizzBuzz():
+    assert fizzBuzz(15) == "FizzBuzz"
+
+
+def test_30_should_return_FizzBuzz():
+    assert fizzBuzz(30) == "FizzBuzz"
+
+
+for i in range(1, 32):
+    print(fizzBuzz(i))
