316772d3886809d433661e7074f58b803022fcfb
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2022-12-22 04:30:57 +0000

    cleanup

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,5 +1,17 @@
 import pytest
 
 
-def test_canAssertTrue():
-    assert True
+def fizzBuzz(value):
+    return value
+
+
+def test_canCallFizzBuzz():
+    fizzBuzz(1)
+
+
+def test_1_should_return_1():
+    assert fizzBuzz(1) == 1
+
+
+def test_2_should_return_2():
+    assert fizzBuzz(2) == 2
