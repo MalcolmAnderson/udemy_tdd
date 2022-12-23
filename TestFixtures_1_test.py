eb93098bb14333b849387f1b2fc644cb62eb4580
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2022-12-23 03:55:54 +0000

    pytest.fixture v5 - using fixture(autouse=True)

--- TestFixtures_1_test.py
+++ TestFixtures_1_test.py
@@ -1,18 +1,17 @@
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def setup():
     print("\nSetup ***********")
 
 
-def test1(setup):
+def test1():
     print("Executing test1 !")
     assert True
     # print("\nLeaving test1!")
 
 
-@pytest.mark.usefixtures("setup")
 def test2():
     print("Executing test2 !")
     assert True
