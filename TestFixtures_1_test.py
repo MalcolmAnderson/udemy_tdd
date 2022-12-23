372597580dd9ea4a98e2239b6a5f9c8dcb87b80a
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2022-12-23 04:17:43 +0000

    pytest.fixture v6 - yield and addfinalizer

--- TestFixtures_1_test.py
+++ TestFixtures_1_test.py
@@ -1,18 +1,35 @@
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def setup():
-    print("\nSetup ***********")
+@pytest.fixture()
+def setup1():
+    # yield
+    print("\nSetup 1  *")
+    yield  # yield is a replacement for return
+    print("\nYield TearDown")
 
 
-def test1():
-    print("Executing test1 !")
+@pytest.fixture()
+def setup2(request):
+    print("\nSetup 2  **")
+
+    def teardown_a():
+        print("\nTeardown A")
+
+    def teardown_b():
+        print("\nTeardown B")
+
+    request.addfinalizer(teardown_a)
+    request.addfinalizer(teardown_b)
+
+
+def test1(setup1):
+    print("\nExecuting test1 !")
     assert True
     # print("\nLeaving test1!")
 
 
-def test2():
-    print("Executing test2 !")
+def test2(setup2):
+    print("\nExecuting test2 !")
     assert True
     # print("\nLeaving test2!")
